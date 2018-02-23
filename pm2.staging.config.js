44b2717fc533df9a72c10364c44db18d0b54b4f4
503945930 <503945930@qq.com>
2018-02-23 16:28:58 +0800

    updat

--- pm2.staging.config.js
+++ pm2.staging.config.js
@@ -6,8 +6,8 @@ module.exports = {
         env: {   // all environment
           'NODE_ENV': 'staging'
         },
-        'instances': 'fork',   // 如果是fork, 不用配置
-        //'exec_mode': 'cluster'  // cluster or fork
+        //'instances': 'fork',   // 如果是fork, 不用配置
+        'exec_mode': 'fork'  // cluster or fork
       }
     ]
   }
